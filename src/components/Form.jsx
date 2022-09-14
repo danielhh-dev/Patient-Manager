85aec1878f3dfb5bef6a1fb105c0d60837446f1a
Daniel Hernandez <danielhh.dev@gmail.com>
2022-09-14 16:04:52 -0500

    Functions to control the form are added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,37 @@
+import { useState } from "react";
+
+const initialForm = {
+  mascota: "",
+  propietario: "",
+  email: "",
+  alta: "",
+  sintomas: "",
+};
+
 const Form = () => {
+  const [form, setForm] = useState(initialForm);
+  const [error, setError] = useState(false);
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (Object.values(form).includes("")) {
+      setError(true);
+      return;
+    }
+
+    setError(false);
+    handleReset();
+  };
+
+  const handleReset = () => setForm(initialForm);
+
   return (
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
@@ -8,76 +41,64 @@ const Form = () => {
         <span className="text-indigo-500 font-bold">Administralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-xl py-10 px-5 mb-10">
+      <form
+        className="bg-white shadow-md rounded-xl py-10 px-5 mb-10"
+        onSubmit={handleSubmit}
+      >
+        {error && (
+          <div className="bg-red-500 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+            <p>Todos los campos son obligatorios</p>
+          </div>
+        )}
         <div className="mb-5">
-          <label
-            htmlFor="mascota"
-            className="block text-gray-700 uppercase font-bold"
-          >
-            Nombre de la Mascota
-          </label>
           <input
-            id="mascota"
+            name="mascota"
             type="text"
-            placeholder="Mascota"
+            placeholder="Nombre de la Mascota"
+            onChange={handleChange}
+            value={form.mascota}
+            className="border-2 w-full p-2 mt-2 rdounded-m placeholder-gray-400"
+          />
+        </div>
+
+        <div className="mb-5">
+          <input
+            name="propietario"
+            type="text"
+            placeholder="Nombre del Propietario"
+            onChange={handleChange}
+            value={form.propietario}
             className="border-2 w-full p-2 mt-2 rounded-md placeholder-gray-400"
           />
         </div>
 
         <div className="mb-5">
-          <label
-            htmlFor="mascota"
-            className="block text-gray-700 uppercase font-bold"
-          >
-            Nombre del Propietario
-          </label>
           <input
-            id="mascota"
-            type="text"
-            placeholder="Propietario"
-            className="border-2 w-full p-2 mt-2 rounded-md placeholder-gray-400"
-          />
-        </div>
-
-        <div className="mb-5">
-          <label
-            htmlFor="email"
-            className="block text-gray-700 uppercase font-bold"
-          >
-            Email de Contacto
-          </label>
-          <input
-            id="email"
+            name="email"
             type="email"
-            placeholder="Email"
+            placeholder="Email de Contacto"
+            onChange={handleChange}
+            value={form.email}
             className="border-2 w-full p-2 mt-2 rounded-md placeholder-gray-400"
           />
         </div>
 
         <div className="mb-5">
-          <label
-            htmlFor="alta"
-            className="block text-gray-700 uppercase font-bold"
-          >
-            Alta
-          </label>
           <input
-            id="alta"
+            name="alta"
             type="date"
+            onChange={handleChange}
+            value={form.alta}
             className="border-2 w-full p-2 mt-2 rounded-md"
           />
         </div>
 
         <div className="mb-5">
-          <label
-            htmlFor="alta"
-            className="block text-gray-700 uppercase font-bold"
-          >
-            Síntomas
-          </label>
           <textarea
-            id="sintomas"
+            name="sintomas"
             placeholder="Describe los Síntomas"
+            onChange={handleChange}
+            value={form.sintomas}
             className="border-2 w-full p-2 mt-2 rounded-md placeholder-gray-400"
           />
         </div>
