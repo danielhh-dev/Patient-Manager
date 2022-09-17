119bdd460ca8efe82c44f1f30a0cf50b75921c1e
Daniel Hernandez <53809074+danielhh-dev@users.noreply.github.com>
2022-09-17 17:39:35 -0500

    Update Formulario.jsx

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -159,7 +159,7 @@ const Formulario = ({ pacientes, setPacientes, paciente, setPaciente }) => {
 
                 <input
                     type="submit"
-                    className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+                    className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors rounded-md"
                     value={ paciente.id ? 'Editar Paciente' : 'Agregar Paciente' }
                 />
             </form>
